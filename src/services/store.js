d98f358d3b76ee83ce29df58a4546ca52e25189d
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-06-01 12:51:00 +0600

    added logged in userInfo in state

--- src/services/store.js
+++ src/services/store.js
@@ -6,6 +6,8 @@ import {
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
+import { userLoginReducer } from "./reducers/userReducers";
+
 import {
   productListReducer,
   productDetailsReducer,
@@ -13,6 +15,7 @@ import {
 import { cartReducer } from "./reducers/cartReducers";
 
 const reducer = combineReducers({
+  userLogin: userLoginReducer,
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
@@ -22,10 +25,15 @@ const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
   },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
