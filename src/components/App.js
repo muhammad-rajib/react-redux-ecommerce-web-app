cb85adaead6c2dec7fb9dccfc25b392fc2a664a9
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-06-06 00:37:45 +0600

    added order page

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,19 @@ import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Header from "./Header";
-import Footer from "./Footer";
 import Home from "../components/pages/Home";
+import Footer from "./Footer";
+
+import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/loginPage";
+import ProfilePage from "./pages/ProfilePage";
+
 import ProductPage from "../components/pages/PorductPage";
 import Cart from "../components/pages/Cart";
-import RegisterPage from "./pages/RegisterPage";
-import ProfilePage from "./pages/ProfilePage";
 import ShippingPage from "./pages/ShippingPage";
 import PaymentPage from "./pages/PaymentPage";
 import PlaceholderPage from "./pages/PlaceholderPage";
+import OrderPage from "./pages/OrderPage";
 
 function App() {
   return (
@@ -25,14 +28,16 @@ function App() {
               <Route path="/register" element={<RegisterPage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/profile" element={<ProfilePage />} />
+
               <Route path="/products/:id" element={<ProductPage />} />
-              <Route path="/shipping" element={<ShippingPage />} />
-              <Route path="/payment" element={<PaymentPage />} />
-              <Route path="/placeholder" element={<PlaceholderPage />} />
               <Route path="/cart">
                 <Route index element={<Cart />} />
                 <Route path=":id" element={<Cart />} />
               </Route>
+              <Route path="/shipping" element={<ShippingPage />} />
+              <Route path="/payment" element={<PaymentPage />} />
+              <Route path="/placeholder" element={<PlaceholderPage />} />
+              <Route path="/order/:id" element={<OrderPage />} />
             </Routes>
           </Container>
         </main>
