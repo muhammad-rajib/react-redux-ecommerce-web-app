06d85ba427fe04db9ca6670bedec22d2a87b07bf
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-06-12 10:06:24 +0600

    added hash route

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import Header from "./Header";
 import Home from "../components/pages/Home";
